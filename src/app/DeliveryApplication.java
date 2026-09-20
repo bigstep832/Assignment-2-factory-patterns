42723699b59d67fc502c41ad3d080c2744f9d7a3
asamgeglk123-hub <asamgeglk123@gmail.com>
2026-09-20 20:13:53 +0500

    add delivery application client

---
+++ src/app/DeliveryApplication.java
@@ -0,0 +1,27 @@
+package app;
+
+import abstractfactory.Button;
+import abstractfactory.Checkbox;
+import abstractfactory.GUIFactory;
+import factorymethod.Logistics;
+
+public class DeliveryApplication {
+
+    private final GUIFactory guiFactory;
+    private final Logistics logistics;
+
+    public DeliveryApplication(GUIFactory guiFactory, Logistics logistics) {
+        this.guiFactory = guiFactory;
+        this.logistics = logistics;
+    }
+
+    public void run(String cargo, String destination) {
+        Button button = guiFactory.createButton();
+        Checkbox checkbox = guiFactory.createCheckbox();
+
+        button.paint();
+        checkbox.paint();
+
+        logistics.planDelivery(cargo, destination);
+    }
+}
